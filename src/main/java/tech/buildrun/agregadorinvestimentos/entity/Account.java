904e4f6f0347ebcae9e5c26aecc3c04e4d7bf3d3
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2025-08-08 20:52:12 -0300

    feat: add entity classes

--- src/main/java/tech/buildrun/agregadorinvestimentos/entity/Account.java
+++ src/main/java/tech/buildrun/agregadorinvestimentos/entity/Account.java
@@ -1,4 +1,65 @@
 package tech.buildrun.agregadorinvestimentos.entity;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_accounts")
 public class Account {
+
+    @Id
+    @Column(name = "account_id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID accountId;
+
+    @OneToOne(mappedBy = "account")
+    @PrimaryKeyJoinColumn
+    private BillingAddress billingAddress;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name ="user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "account")
+    private List<AccountStock> accountStocks;
+
+
+
+    public Account() {
+
+    }
+
+    public Account(UUID accountId, String description) {
+        this.accountId = accountId;
+        this.description = description;
+    }
+
+    public UUID getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(UUID accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
